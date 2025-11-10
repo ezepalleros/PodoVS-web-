ca57d34d6628903627510b39987853190a369a3a
Ezequiel Germán Palleros Ibarra <ezequiel.palleros@davinci.edu.ar>
2025-11-10 03:21:26 -0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -59,7 +59,7 @@ $ADMIN_NONCE = $_SESSION['admin_login_nonce'];
 
   <script>
   const firebaseConfig = {
-    apiKey: "AIzaSyAH-grTjHt4KxqAwuf6lY_Z5Eh_y0FNYR0",
+    apiKey: "NADADENADA",
     authDomain: "podovs-ba062.firebaseapp.com",
     projectId: "podovs-ba062",
     storageBucket: "podovs-ba062.appspot.com",
